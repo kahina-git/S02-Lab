f14a14207fc0c1262ea83ccf119ddc1bbc1ac0f4
kahina-git <77074219+kahina-git@users.noreply.github.com>
2024-05-30 05:28:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# S02_Lab1
+Révision 2W5: ViewsModels, annotations de validation et taghelpers avec singleton (listes de données)
---
+++ ZombieParty/ZombieParty/Controllers/ZombieController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZombieParty.Models;
+
+namespace ZombieParty.Controllers
+{
+    public class ZombieController : Controller
+    {
+        public IActionResult Index()
+        {
+            this.ViewBag.MaListe = new List<Zombie>();
+
+            return View();
+        }
+    }
+}
