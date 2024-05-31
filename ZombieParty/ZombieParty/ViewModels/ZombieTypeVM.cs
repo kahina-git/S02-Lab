a5a65569ee6962844850caeae4958af2f2c92c34
Kahina <kahina.ghidouche@cegepmontpetit.ca>
2024-05-31 15:08:20 -0400

    FCT Création View ZombieType Details
    Ajout de la view Details affichant le ZombieType et la liste des zombies associés

---
+++ ZombieParty/ZombieParty/ViewModels/ZombieTypeVM.cs
@@ -0,0 +1,11 @@
+﻿using ZombieParty.Models;
+
+namespace ZombieParty.ViewModels
+{
+    public class ZombieTypeVM
+    {
+        public ZombieType ZombieType { get; set; }
+        public List<Zombie> ZombiesList { get; set; } = new List<Zombie>();
+
+    }
+}
