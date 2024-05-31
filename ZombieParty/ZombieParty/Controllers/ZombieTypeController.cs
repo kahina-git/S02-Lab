812530b77e03d4c4d83a926297c376a9c6fe7bc8
Kahina <kahina.ghidouche@cegepmontpetit.ca>
2024-05-31 09:56:07 -0400

    FCT ZombieType Ajout Singleton Create et Validation
    Ajout de l’utilisation du Singleton et de la validation côté client et côté serveur pour ZombieType.

--- ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
@@ -5,12 +5,21 @@ namespace ZombieParty.Controllers
 {
     public class ZombieTypeController : Controller
     {
+
+        private BaseDonnees _baseDonnees { get; set; }
+
+        public ZombieTypeController(BaseDonnees baseDonnees)
+        {
+            _baseDonnees = baseDonnees;
+        }
+
+
         public IActionResult Index()
         {
-            this.ViewBag.MaListe = new List<ZombieType>()
+            this.ViewBag.MaListe = _baseDonnees.ZombieTypes.ToList();
             {
-                new ZombieType(){TypeName= "Virus", Id=1},
-                new ZombieType(){TypeName= "Contact", Id=2}
+                //new ZombieType(){TypeName= "Virus", Id=1},
+                //new ZombieType(){TypeName= "Contact", Id=2}
             };
 
             return View();
@@ -29,6 +38,9 @@ namespace ZombieParty.Controllers
             if (ModelState.IsValid)
             {
                 // Ajouter à la BD
+                _baseDonnees.ZombieTypes.Add(zombieType);
+                return this.RedirectToAction("Index");
+
             }
 
             return this.View(zombieType);
--- ZombieParty/ZombieParty/Program.cs
+++ ZombieParty/ZombieParty/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 //// Injection de dépendance du singleton 
 builder.Services.AddSingleton<BaseDonnees>();
 
